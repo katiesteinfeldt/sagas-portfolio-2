4ede91c84c4bd44a0f97b2ebe2f14cf8dd6735b7
Katie Mangan <katie.mangan1@gmail.com>
2019-03-09 14:10:11 -0600

    remove material ui

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './ProjectItem.css';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-// import Button from '@material-ui/core/Button';
-import { CardContent, CardActions, Divider } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import Typo from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import { BrowserRouter as Router } from "react-router-dom";
-
-const styles = {
-    card: {
-        minWidth: 275,
-        maxWidth: 350,
-        margin: 22,
-    }
-}
 
 class ProjectItem extends Component {
 
@@ -58,25 +43,19 @@ class ProjectItem extends Component {
         }
 
         return (
-            <Grid>
-                <Card className={this.props.classes.card}>
-                    <CardContent>
-                        <Typo>{this.props.project.name}</Typo>
-                        <Divider />
-                        <Typo>{descriptionDisplay}</Typo>
-                        <br />
-                        {thumbnailDisplay}
-                    </CardContent>
-                    <CardActions>
-                        <Router>
-                            <Button><a href={websiteDisplay}>Website</a></Button>
-                        </Router> 
-                        <Router>
-                            <Button><a href={githubDisplay}>Github</a></Button>
-                        </Router>
-                    </CardActions>
-                </Card>
-            </Grid>
+            <div>
+                <div>
+                    {this.props.project.name}
+                    <br/>
+                    {descriptionDisplay}
+                    <br />
+                    {thumbnailDisplay}
+                    <br />
+                    {websiteDisplay}
+                    <br />
+                    {githubDisplay}
+                </div>
+            </div>
         );
     }
 }
@@ -89,4 +68,4 @@ const mapStateToProps = (reduxState) => {
     return reduxState;
 }
 
-export default withStyles(styles)(connect(mapStateToProps)(ProjectItem));
+export default connect(mapStateToProps)(ProjectItem);
